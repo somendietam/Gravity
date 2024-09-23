7d8e985b670cc0e9d6302bb38e3353178848426f
Bryan <zuluagabrayan099@gmail.com>
2024-09-23 17:32:22 -0500

    Eliminar .env del repositorio

--- gravity_project/gravity_app/views.py
+++ gravity_project/gravity_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Producto, CarritoCompras, Cliente
 
 def index(request):
     context = {}
     return render(request, 'gravity_app/index.html', context)
+
+
+
