e8c2917e2148466ab1a08f73a6e6efa339570157
Bryan <zuluagabrayan099@gmail.com>
2024-09-24 17:05:10 -0500

    listo

--- gravity_project/gravity_app/admin.py
+++ gravity_project/gravity_app/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import Categoria, Producto, CarritoCompras, Cliente, Pedido, PedidoProducto
 
-# Registra los modelos en el panel de administración
 admin.site.register(Categoria)
-admin.site.register(Producto)
 admin.site.register(CarritoCompras)
 admin.site.register(Cliente)
 admin.site.register(Pedido)
